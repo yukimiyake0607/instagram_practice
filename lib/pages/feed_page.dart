ad02a3d1df69836292cff9fb2b180c54cb7866fb
yukimiyake0607 <yukimiyake38@gmail.com>
2024-04-11 18:46:28 +0900

    フィードページのappbarを追加

--- lib/pages/feed_page.dart
+++ lib/pages/feed_page.dart
@@ -7,7 +7,22 @@ class FeedPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('フィード'),
+        title: const Column(
+          children: [
+            Text(
+              'instagram',
+              style: TextStyle(color: Colors.grey),
+            ),
+            Text(
+              '投稿',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+          ],
+        ),
+        shape: Border(bottom: BorderSide(color: Colors.grey, width: 1)),
       ),
       body: Center(child: Text('フィード')),
     );
